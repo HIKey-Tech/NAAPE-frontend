0a4b5140320e587d0d919c4661e7b15b78fc41be
King-Lota <lottanna47@gmail.com>
2025-11-23 21:42:21 +0100

    feat: implement news and notification

--- app/(private)/(admin)/admin/dashboard/page.tsx
+++ app/(private)/(admin)/admin/dashboard/page.tsx
@@ -1,16 +1,9 @@
-"use client";
 
 import AdminOverview from "@/components/admin/overview";
-import MemberDashboardHome from "@/components/member/dashboard/home";
-import { useAuth } from "@/context/authcontext";
-import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
-    const router = useRouter();
-    const { user } = useAuth();
 
 
-    console.log("user details:", user?.name.toString())
 
     return (
         <main className="">
