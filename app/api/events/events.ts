1bb3860f5f098b2d1cf5f61cfadbe90420c54cdd
King-Lota <lottanna47@gmail.com>
2025-12-01 11:52:53 +0100

    update env configurations

--- app/api/events/events.ts
+++ app/api/events/events.ts
@@ -16,6 +16,7 @@ export const createEventApi = async (data: FormData) => {
                 Authorization: `Bearer ${token}`
             }
         })
+        
         return response.data
 
     } catch (error) {
