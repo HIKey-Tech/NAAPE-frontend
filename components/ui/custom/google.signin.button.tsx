227d196cffb367faffc447d1c540e3ea37d40ee4
wolethescientist <davidaiyewumi1@gmail.com>
2026-02-05 15:34:04 +0100

    fix

--- components/ui/custom/google.signin.button.tsx
+++ components/ui/custom/google.signin.button.tsx
@@ -61,7 +61,6 @@ export default function GoogleSignInButton({
                 text={text}
                 shape="rectangular"
                 size="large"
-                width="100%"
                 theme="outline"
             />
         </div>
