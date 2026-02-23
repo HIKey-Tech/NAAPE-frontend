f98ca6378d4fdd0ab15d3b58a5d8f7f1ead707c0
wolethescientist <davidaiyewumi1@gmail.com>
2026-02-23 10:30:54 +0100

    fix

--- app/(public)/publication/naape/[id]/page.tsx
+++ app/(public)/publication/naape/[id]/page.tsx
@@ -47,14 +47,6 @@ export default function NaapePublicationDetailPage() {
         const data = user 
           ? await getSinglePublication(id)
           : await getSinglePublicationPublic(id);
-        
-        console.log("=== FRONTEND PUBLICATION DEBUG ===");
-        console.log("User logged in:", !!user);
-        console.log("Publication data:", data);
-        console.log("isPreview:", data.isPreview);
-        console.log("requiresSubscription:", data.requiresSubscription);
-        console.log("Content length:", data.content?.length);
-        
         setPublication(data);
       } catch (err: any) {
         console.error("Failed to fetch publication:", err);
